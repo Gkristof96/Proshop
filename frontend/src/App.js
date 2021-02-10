59c73686fed5457c167c3fcbf4f9530ccdd7d78c
Gkristof96 <kristef.g@gmail.com>
2021-02-10 15:33:08 +0100

    resetup all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-      Hello there
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen}/>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
